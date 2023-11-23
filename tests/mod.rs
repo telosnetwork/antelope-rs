a69199b3a4010c6e28edf1393f8cd2690bae109c
Jesse Schulman <jesse@caleos.io>
2023-11-23 12:29:08 -0800

    Bring in whole antelope ts script, commented out

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,8 +1,3 @@
 #[cfg(test)]
 
-// integration
 mod serializer;
-
-// chain
-// Copy values and try to match tests from WharfKit: https://github.com/wharfkit/antelope/blob/master/test/serializer.ts
-mod name;
